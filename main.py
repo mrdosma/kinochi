22343fc8e20efa80fdc1764d482dd0e184d6fd9a
Mr.DosMa <102180720+mrdosma@users.noreply.github.com>
2025-11-17 15:19:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-from flask import flask
-import threading
 import telebot
+from flask import Flask
 from telebot import types
 
-app = flask(__name__)
+app = Flask(__name__)
+
+
 @app.route("/", methods=["GET"])
-def home():
-return "Salom bot ishlayapti", 200
+def hello():
+    return "Salom bot ishlayapti", 200
+
 
 # BOT tokeni
 TOKEN = "8413767557:AAFslk-Ej9KRX3XgU5vHArRErr348HQkcGs"
 bot = telebot.TeleBot(TOKEN)
 
+
 # Botni kodlari
 
 
-@bot.message_handler (commands=["start"])
-def start (msg):
-bot.reply_to (msg, "Salom, bot ishlavotd ! ")
+@bot.message_handler(commands=["start"])
+def start(msg):
+    bot.reply_to(msg, "Salom, bot ishlavotd ! ")
+
 
 # obuna bolish kerak bolgan kanallar
 CHANNELS = [
-    "@ownclouds","@kinolar_doramalar_uz" , "@AnimeWorldCafe"
+    "@ownclouds", "@kinolar_doramalar_uz", "@AnimeWorldCafe"
 ]
+
 
 # User kanallaga obuna bolganmi yoki yoq tekshrsh
 def check_user(user_id):
@@ -35,14 +40,16 @@ def check_user(user_id):
             return False
     return True
 
+
 # databasaga saqlash
 from pymongo import MongoClient
 
-MONGO_URL = "https://downloads.mongodb.com/compass/mongosh-2.5.9-x64.msi" ,"mongosh "mongodb+srv://kinochi.l0u51kz.mongodb.net/" --apiVersion 1 --username mrdosma_db_user --password s2N4YIv2KIaDT4v7"
+MONGO_URL = "mongodb+srv://kinochi.l0u51kz.mongodb.net/"
 client = MongoClient(MONGO_URL)
 
 db = client["kinochi_bot"]
 collection = db["videos"]
+
 
 @bot.channel_post_handler(content_types=["video"])
 def handle_channel_post(message):
@@ -52,6 +59,7 @@ def handle_channel_post(message):
             "file_id": message.video.file_id,
             "caption": message.caption
         })
+
 
 # obuna bolmagan bolsa sorov berish
 def ask_to_subscribe(chat_id):
@@ -64,6 +72,7 @@ def ask_to_subscribe(chat_id):
 
     bot.send_message(chat_id, "Botdan foydalanish uchun kanallarga obuna boling", reply_markup=markup)
 
+
 # start bosilganda
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -74,6 +83,7 @@ def start(message):
         # agar obuna bolmagan bolsa
         ask_to_subscribe(message.chat.id)
 
+
 # tekshrsh tugmasi bosilganda
 @bot.callback_query_handler(func=lambda call: call.data == "check")
 def check_callback(call):
@@ -82,6 +92,7 @@ def check_callback(call):
         bot.send_message(call.message.chat.id, "Botdan foydalanishingiz mumkin!")
     else:
         bot.send_message(call.message.chat.id, "Barcha kanallarga obuna bolmagansiz!")
+
 
 # har qanday habar kelganda tekshrsh
 @bot.message_handler(func=lambda message: True)
@@ -95,13 +106,12 @@ def all_messages(message):
         for video in collection.find():
             if f"Kod: {message.text}" in video["caption"]:
                 bot.send_video(
-                    message.chat.id, 
-                    video["file_id"], 
+                    message.chat.id,
+                    video["file_id"],
                     caption=video["caption"]
                 )
     else:
         bot.send_message(message.chat.id, "Noto'g'ri formatdagi kod")
 
+
 bot.polling()
-
-
